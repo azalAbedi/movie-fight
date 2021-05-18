718f3b86de612931315814ae8c3c4092d3e5202e
Azal Abedi <azal.abedi@gmail.com>
2021-05-18 15:44:36 -0600

    add styling after a comparison is run to indicate each win per metric category, complete version 1.0

--- index.js
+++ index.js
@@ -66,8 +66,24 @@ const onMovieSelect = async (movie, summaryElement, side) => {
     }
 };
 
-const runComparison = () => {
-    console.log('Compare time!'); // FINISH this next!
+const runComparison = () => { // ROOM for IMPROVEMENT -- add a tally and display a banner or signifier on the movie that won
+    const leftSideStats = document.querySelectorAll('#left-summary .notification');
+    const rightSideStats = document.querySelectorAll('#right-summary .notification');
+
+    leftSideStats.forEach((leftStat, index) => {
+        const rightStat = rightSideStats[index]; // this is piggy-backing so we don't need to write another forEach for the right side
+    
+        const leftSideValue = parseInt(leftStat.dataset.value); // <-- use parseInt on these becasue dataset values are strings by default
+        const rightSideValue = parseInt(rightStat.dataset.value);
+
+        if (rightSideValue > leftSideValue) {
+            leftStat.classList.remove('is-primary');
+            leftStat.classList.add('is-warning');
+        } else {
+            rightStat.classList.remove('is-primary');
+            rightStat.classList.add('is-warning');
+        }
+    });
 };
 
 const movieTemplate = movieDetail => {
