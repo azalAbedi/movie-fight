709669f07c05a070b7531395af9764f037f61788
Azal Abedi <azal.abedi@gmail.com>
2021-05-17 19:15:03 -0600

    set up OMDb API via simple input HTML tag

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'b2fd1c5d',
+            s: searchTerm
+        }
+    });
+
+    console.log(response.data);
+};
+
+const input = document.querySelector('input');
+input.addEventListener('input', (event) => {
+    fetchData(event.target.value);
+});
