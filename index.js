a683d28812b21e326994e9d75f2704322afe5bee
Azal Abedi <azal.abedi@gmail.com>
2021-05-17 19:57:52 -0600

    display poster and title from a search and catch error when a movie is not found

--- index.js
+++ index.js
@@ -6,12 +6,27 @@ const fetchData = async (searchTerm) => {
         }
     });
 
-    console.log(response.data);
+    if (response.data.Error) {
+        return [];
+    } // This is for if "movie not found" happens with the OMDb API
+
+    return response.data.Search;
 };
 
 const input = document.querySelector('input');
 
-const onInput = (event) => {
-    fetchData(event.target.value);
+const onInput = async (event) => {
+    const movies = await fetchData(event.target.value);
+    
+    for (let movie of movies) {
+        const div = document.createElement('div');
+        
+        div.innerHTML = `
+            <img src="${movie.Poster}" />
+            <h2>${movie.Title}</h2>
+        `;
+
+        document.querySelector('#target').appendChild(div);
+    }
 };
 input.addEventListener('input', debounce(onInput, 500));
